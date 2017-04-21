3a70c243d68c15d75dbb677b9faa4803d7405dab
liutao <lt15196622549@gmail.com>
2017-04-21 20:45:22 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'jobs#index'
+
 
   namespace :admin do
     resources :jobs do
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
 
@@ -17,9 +18,6 @@ Rails.application.routes.draw do
     resources :resumes
   end
 
-
-
-
-
+  root 'jobs#index'
 
 end
